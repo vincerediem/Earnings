07d73fd3982a6e020ea3b1ddb43fcec4bd6a5449
Keith <kopenshaw@me.com>
2023-06-23 21:34:06 -0700

    takes stock input list

--- Earningsv1_av.py
+++ Earningsv1_av.py
@@ -2,11 +2,29 @@ import requests
 
 alpha_vantage_key = 'Y84MMO9G796B0PNO'
 
-url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=Y84MMO9G796B0PNO'
-r = requests.get(url)
-data = r.json()
+def stock_list():
+    input_str = input("Enter stocks separated by space: ")
+    # Split the string and convert each value to integer, creating an array
+    stock_list = input_str.split()
+    return stock_list
 
-annual_earnings = data['annualEarnings']
-quarterly_earnings = data['quarterlyEarnings']
+stock_list = stock_list()
 
-print([float(earning["reportedEPS"]) for earning in annual_earnings])
+def earnings_history(stock_list):
+    for stock in stock_list:
+        url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=' + stock + '&apikey=Y84MMO9G796B0PNO'
+        r = requests.get(url)
+        data = r.json()
+
+        annual_earnings = data['annualEarnings']
+        #contains 'reportedEPS' and 'fiscalDateEnding'
+
+        quarterly_earnings = data['quarterlyEarnings']
+
+        print(stock.upper())
+        for i in annual_earnings:
+            print(f"{float(i['reportedEPS'])}% on {i['fiscalDateEnding']}")
+
+    return annual_earnings, quarterly_earnings
+    
+annual_earnings, quarterly_earnings = earnings_history(stock_list)
